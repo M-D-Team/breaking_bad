0a979eba34ea3796ef449c8b9b5d1aaa084c2c22
Duzo <james.hall01@outlook.com>
2023-02-18 20:53:54 +0000

    test of gas mask

--- src/main/java/com/mdt/breakbad/core/init/BreakBadItems.java
+++ src/main/java/com/mdt/breakbad/core/init/BreakBadItems.java
@@ -1,10 +1,14 @@
 package com.mdt.breakbad.core.init;
 
+import com.mdt.breakbad.common.BreakBadArmourMaterials;
 import com.mdt.breakbad.common.items.FulminateItem;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,4 +21,5 @@ public class BreakBadItems {
 
     public static final RegistryObject<Item> METH = ITEMS.register("meth", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,4000,1),1f).build())));
     public static final RegistryObject<Item> FULMINATE = ITEMS.register("fulminate", () -> new FulminateItem(new Item.Properties()));
+    public static final RegistryObject<Item> GAS_MASK = ITEMS.register("gas_mask", () -> new ArmorItem(BreakBadArmourMaterials.GAS_MASK, EquipmentSlot.HEAD,new Item.Properties()));
 }
