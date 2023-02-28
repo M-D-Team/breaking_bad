1a2cc8b5b950dcf8d71bf811f82aa06813d1d685
Duzo <james.hall01@outlook.com>
2023-02-28 02:38:56 +0000

    made meth do more stuff

---
+++ src/main/java/com/mdt/breakbad/common/items/MethItem.java
@@ -0,0 +1,26 @@
+package com.mdt.breakbad.common.items;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class MethItem extends Item {
+    public MethItem(Properties properties) {
+        super(properties);
+    }
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        ItemStack itemStack = pPlayer.getItemInHand(pHand);
+        Item item = itemStack.getItem();
+
+
+
+        return super.use(pLevel, pPlayer, pHand);
+    }
+}
--- src/main/java/com/mdt/breakbad/core/init/BreakBadItems.java
+++ src/main/java/com/mdt/breakbad/core/init/BreakBadItems.java
@@ -2,11 +2,13 @@ package com.mdt.breakbad.core.init;
 
 import com.mdt.breakbad.common.BreakBadArmourMaterials;
 import com.mdt.breakbad.common.items.FulminateItem;
+import com.mdt.breakbad.common.items.MethItem;
 import com.mdt.breakbad.common.items.WearableItem;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
@@ -19,8 +21,22 @@ import static com.mdt.breakbad.BreakBad.MODID;
 public class BreakBadItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    public static final RegistryObject<Item> METH = ITEMS.register("meth", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,4000,1),1f).build())));
-    public static final RegistryObject<Item> FLAVOURING_EXTRACT = ITEMS.register("flavouring_extract", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().effect(new MobEffectInstance(MobEffects.DARKNESS,4000,1),1f).build())));
+    public static final RegistryObject<Item> METH = ITEMS.register("meth", () -> new MethItem(new Item.Properties().food(new FoodProperties.Builder()
+             // POSITIVE EFFECTS
+            .effect(new MobEffectInstance(MobEffects.DIG_SPEED, 3600,1),0.25f)
+            .effect(new MobEffectInstance(MobEffects.JUMP,3600,1),0.25f)
+            .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,3600,1),0.25f)
+            .effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST,3600,1),0.5f)
+            .effect(new MobEffectInstance(MobEffects.LUCK,3600,1),0.25f)
+            .effect(new MobEffectInstance(MobEffects.REGENERATION,3600,1),0.5f)
+             // NEGATIVE EFFECTS
+            .effect(new MobEffectInstance(MobEffects.LEVITATION,300,1),0.5f)
+            .effect(new MobEffectInstance(MobEffects.POISON,600,1),0.5f)
+            .effect(new MobEffectInstance(MobEffects.HUNGER,600,1),0.5f)
+            .effect(new MobEffectInstance(MobEffects.CONFUSION,600,1),0.5f)
+            .effect(new MobEffectInstance(MobEffects.DARKNESS,1200,1),0.75f)
+            .build())));
+    public static final RegistryObject<Item> FLAVOURING_EXTRACT = ITEMS.register("flavouring_extract", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().effect(new MobEffectInstance(MobEffects.DARKNESS,200,1),1f).build())));
     public static final RegistryObject<Item> AMETHYST_DUST = ITEMS.register("amethyst_dust", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BREAKING_BREAD = ITEMS.register("breaking_bread", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(99).saturationMod(99).build())));
     public static final RegistryObject<Item> FULMINATE = ITEMS.register("fulminate", () -> new FulminateItem(new Item.Properties()));
